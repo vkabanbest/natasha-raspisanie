875c1169f19f858c8e8621ad1550eceac1612f00
vkabanbest <vkaban13@gmail.com>
2026-09-22 21:22:30 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ru.natasha.schedule',
+  appName: 'Расписание Наташи',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
